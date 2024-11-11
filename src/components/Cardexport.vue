a2c6c4ccd028bde8353e2395c6d0d8cc24a67e4b
Marlene <ruthboko23@gmail.com>
2024-11-11 11:03:31 +0100

    Last test

--- src/components/Cardexport.vue
+++ src/components/Cardexport.vue
@@ -14,7 +14,7 @@ const props = defineProps({
 <template>
     <div class="bg-[#eee] p-5 min-h-screen w-full">
         <div :class="cardOrientation === 'vertical' ? 'flex flex-col items-center gap-[50px]' : 'flex flex-col items-center justify-center gap-[50px]'">
-          <div style="box-shadow: 0 0 80px #0006;"  >
+          <div style="box-shadow: 0 0 80px #0006;"  :class="cardOrientation === 'vertical' ? 'w-[250px] h-full' : 'w-[400px] h-[250px]'" >
             <img :src="rectoImage" alt="Recto" class="max-w-full max-h-full rounded-lg sm:rounded-lg lg:rounded-lg"   :class="cardOrientation === 'vertical' ? 'w-[250px] h-full' : 'w-[400px] h-[250px]'"/>
 
           </div>
